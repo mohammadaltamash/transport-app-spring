300b15ce17cf006ac61abf6a2a092808e8a4f7dd
Mohammad Altamash <muhammad.altamesh@gmail.com>
2020-03-03 16:43:23 +0500

    Allow CORS on all requests

--- src/main/java/com/transport/app/rest/controller/OrderController.java
+++ src/main/java/com/transport/app/rest/controller/OrderController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/order")
 public class OrderController {
@@ -19,7 +20,7 @@ public class OrderController {
         this.orderService = orderService;
     }
 
-    @CrossOrigin
+//    @CrossOrigin
 //    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/create")
     public OrderDto create(@Valid @RequestBody Order order) {
