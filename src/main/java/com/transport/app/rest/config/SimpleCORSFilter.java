c586a81f4762e3a45cad06b38381fbedb2f6c793
Mohammad Altamash <ultamesh@gmail.com>
2020-05-25 19:00:35 +0500

    Update

--- src/main/java/com/transport/app/rest/config/SimpleCORSFilter.java
+++ src/main/java/com/transport/app/rest/config/SimpleCORSFilter.java
@@ -1,6 +1,7 @@
 package com.transport.app.rest.config;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
@@ -12,6 +13,9 @@ import java.io.IOException;
 public class SimpleCORSFilter implements Filter {
 
     private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(SimpleCORSFilter.class);
+    @Value("${access.control.allow.origin}")
+    private String accessControlAllowOrigin;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         LOGGER.info("Initilisation du Middleware");
@@ -22,7 +26,7 @@ public class SimpleCORSFilter implements Filter {
         HttpServletRequest requestToUse = (HttpServletRequest)servletRequest;
         HttpServletResponse responseToUse = (HttpServletResponse)servletResponse;
 
-        responseToUse.setHeader("Access-Control-Allow-Origin","http://localhost:4200");
+        responseToUse.setHeader("Access-Control-Allow-Origin",accessControlAllowOrigin);
         filterChain.doFilter(requestToUse,responseToUse);
     }
 
