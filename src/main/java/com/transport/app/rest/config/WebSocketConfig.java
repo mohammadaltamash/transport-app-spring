0792ef53cde6dfd23355c8231062c9b36278e940
Mohammad Altamash <ultamesh@gmail.com>
2020-05-25 19:12:15 +0500

    Update

--- src/main/java/com/transport/app/rest/config/WebSocketConfig.java
+++ src/main/java/com/transport/app/rest/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.transport.app.rest.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,6 +10,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Value("${access.control.allow.origin}")
+    private String accessControlAllowOrigin;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -21,8 +25,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 //        registry.addEndpoint("/ws")
 //                .setAllowedOrigins("http://localhost:4200");
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("*")
-//                .setAllowedOrigins("http://localhost:4200")
+//                .setAllowedOrigins("*")
+                .setAllowedOrigins(accessControlAllowOrigin)
                 .withSockJS()
         ;
     }
