4af146e44577338cec7d03e2e63bb465c155b450
Mohammad Altamash <ultamesh@gmail.com>
2020-07-28 18:37:22 +0500

    Update

--- src/main/java/com/transport/app/rest/config/WebSocketConfig.java
+++ src/main/java/com/transport/app/rest/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setApplicationDestinationPrefixes("/app");
     }
 
-    @Overrideit co
+    @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 //        registry.addEndpoint("/ws")
 //                .setAllowedOrigins("http://localhost:4200");
